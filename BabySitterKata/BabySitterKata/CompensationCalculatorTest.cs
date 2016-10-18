7d839099e29f8df63abf80bc791568e11af18e9b
trual <trual4@gmail.com>
2016-10-18 12:25:56 -0400

    cleaned test up a bit

--- BabySitterKata/BabySitterKata/CompensationCalculatorTest.cs
+++ BabySitterKata/BabySitterKata/CompensationCalculatorTest.cs
@@ -10,25 +10,32 @@ namespace BabySitterKata
     [TestClass()]
     public class CompensationCalculatorTest
     {
+
+        
         public CompensationCalculatorTest()
         {
 
         }
 
+        
+        CompensationCalculator Compensation = new CompensationCalculator();
+        int start = 0;
+        int end = 0;
+        
+
+
         [TestMethod()]
         public void WhenCCIsPassedAStartTimeAndEndTime()
         {
-            CompensationCalculator Compensation = new CompensationCalculator();
-            int start = 5;
-            int end = 6;
+            start = 5;
+            end = 6;
             Assert.AreEqual(12m, Compensation.calc(start, end));
         }
         [TestMethod()]
         public void WhenBabySitterStaysTillBedtime()
         {
-            CompensationCalculator Compensation = new CompensationCalculator();
-            int start = 5;
-            int end = 9;
+            start = 5;
+            end = 9;
             //making bedtime 9 pm because that's a reasonable time a child should
             // go to bed
             Assert.AreEqual(48m, Compensation.calc(start, end));
@@ -36,18 +43,16 @@ namespace BabySitterKata
         [TestMethod()]
         public void WhenBabySitterStayTillMidnight()
         {
-            CompensationCalculator Compensation = new CompensationCalculator();
-            int start = 5;
-            int end = 12;
+            start = 5;
+            end = 12;
             Assert.AreEqual(72m, Compensation.calc(start, end));
 
         }
         [TestMethod()]
         public void WhenBabySitterStaysTillEndOfJob()
         {
-            CompensationCalculator Compensation = new CompensationCalculator();
-            int start = 5;
-            int end = 4;
+            start = 5;
+            end = 4;
             Assert.AreEqual(136m, Compensation.calc(start, end));
         }
     }

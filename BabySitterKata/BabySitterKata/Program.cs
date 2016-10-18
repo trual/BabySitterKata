f65802ce33a8431cdc364fb3a815529748cc7278
trual <trual4@gmail.com>
2016-10-18 12:58:18 -0400

    Wrote main, to use the program in the console

--- BabySitterKata/BabySitterKata/Program.cs
+++ BabySitterKata/BabySitterKata/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace BabySitterKata
 {
@@ -10,7 +11,23 @@ namespace BabySitterKata
     {
         static void Main(string[] args)
         {
+            CompensationCalculator Compensation = new CompensationCalculator();
+            string x = "";
 
+            while (x != "x")
+            {
+                Console.WriteLine("Calculate Baby sitter's Compensation");
+                Console.WriteLine("Please insert Start hour, only the hour value: ");
+                int start = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please insert end hour, only the hour value: ");
+                int end = Convert.ToInt32(Console.ReadLine());
+                decimal comp = Compensation.calc(start, end);
+                Console.WriteLine("The Baby sitter is due $ " + comp);
+                Console.WriteLine();
+                Console.WriteLine("insert 'x' to close any other key to continue");
+                x = Console.ReadLine();
+            }
+            
         }
     }
 }

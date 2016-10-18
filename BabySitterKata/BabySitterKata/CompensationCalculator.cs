81ce1308c13ea943e5691bb40854c6abe6af1286
trual <trual4@gmail.com>
2016-10-18 01:33:13 -0400

    Initial BabySitter Kata Commit
    Base test

---
+++ BabySitterKata/BabySitterKata/CompensationCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabySitterKata
+{
+    public class CompensationCalculator
+    {
+        public CompensationCalculator()
+        {
+        }
+
+        public decimal calc(int start, int end)
+        {
+            return 0.0m;
+        }
+    }
+}
---
+++ BabySitterKata/BabySitterKata/CompensationCalculatorTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabySitterKata
+{
+    [TestClass()]
+    public class CompensationCalculatorTest
+    {
+        public CompensationCalculatorTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void WhenCCIsPassedAStartTimeAndEndTime()
+        {
+            CompensationCalculator Compensation = new CompensationCalculator();
+            int start = 5;
+            int end = 6;
+            Assert.AreEqual(0.0m, Compensation.calc(start, end));
+        }
+
+    }
+}
